cad0e84c727052663eaf6fddd1594e38a1fec0c8
brandtoliver <obrandt2311@gmail.com>
2025-02-27 11:02:57 +0100

    remove blog and social image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -50,7 +50,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    //image: 'img/docusaurus-social-card.jpg',
     navbar: {
       logo: {
         alt: 'Subsets Logo',
